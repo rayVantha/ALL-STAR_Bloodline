1d0a9c924bc61c0b3b4cfe46c0bd9f98fc102912
vantha <jpradas713@gmail.com>
2025-06-26 16:07:10 -0600

    Stand Arrows arrow behaviour test

--- src/main/java/net/rayVantha/allstarbloodline/item/ModItems.java
+++ src/main/java/net/rayVantha/allstarbloodline/item/ModItems.java
@@ -1,5 +1,6 @@
 package net.rayVantha.allstarbloodline.item;
 
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +13,7 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, AllStarBloodline.MOD_ID);
 
     public static final RegistryObject<Item> STANDARROW = ITEMS.register("standarrow",
-        () -> new Item(new Item.Properties()));
+        () -> new ArrowItem(new Item.Properties()));
 
 
 
