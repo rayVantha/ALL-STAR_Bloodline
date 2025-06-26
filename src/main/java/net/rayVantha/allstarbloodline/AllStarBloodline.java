4ed0ed1079ef52d325dcd8e8d5deac477d7226bc
vantha <jpradas713@gmail.com>
2025-06-26 15:51:57 -0600

    First Commits

--- src/main/java/net/rayVantha/allstarbloodline/AllStarBloodline.java
+++ src/main/java/net/rayVantha/allstarbloodline/AllStarBloodline.java
@@ -1,6 +1,7 @@
 package net.rayVantha.allstarbloodline;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.rayVantha.allstarbloodline.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -29,7 +31,7 @@ public class AllStarBloodline {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -38,12 +40,13 @@ public class AllStarBloodline {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-
     }
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(ModItems.STANDARROW);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/rayVantha/allstarbloodline/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.rayVantha.allstarbloodline.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.rayVantha.allstarbloodline.AllStarBloodline;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, AllStarBloodline.MOD_ID);
+
+    public static final RegistryObject<Item> STANDARROW = ITEMS.register("standarrow",
+        () -> new Item(new Item.Properties()));
+
+
+
+     public static void register(IEventBus eventBus) {
+         ITEMS.register(eventBus);
+     }
+}
